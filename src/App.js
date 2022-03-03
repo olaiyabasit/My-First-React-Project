0719cc12c53fccc17d79b666177fce723fd417f2
Basit Olaiya <bolaiya@wazobia.tech>
2022-03-03 23:31:21 +0100

    my first project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState();
+
+  const updateCalc = (value) => {
+    if (
+      (operators.includes(value) && calc === "") ||
+      (operators.includes(value) && operators.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+    setCalc(calc + value);
+    if (!operators.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  };
+
+  const operators = ["/", "*", "-", "+"];
+  const otherOperand = ["0", "."];
+  
+  const displayOtherOperand = () =>
+    otherOperand.map((operand, key) => (
+      <button key={key} onClick={() => updateCalc(operand.toLowerCase())}>
+        {operand}
+      </button>
+    ));
+
+  const createDigits = () => {
+    const digits = [];
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button key={i} onClick={() => updateCalc(i.toString())}>
+          {i}
+        </button>
+      );
+    }
+
+    return digits;
+  };
+
+  const calculate = () => setCalc(eval(calc).toString());
+
+  const deleteLast = () => {
+    if (calc == "") {
+      return;
+    }
+    const value = calc.slice(0, -1);
+    setCalc(value);
+  };
+
+  const displayOperands = () =>
+    operators.map((operand, index) => (
+      <button key={index} onClick={() => updateCalc(operand.toLowerCase())}>
+        {operand}
+      </button>
+    ));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="display">
+          {result ? <span>{result}</span> : ""} &nbsp;
+          {calc || 0}
+        </div>
+        <div className="operators">
+          {displayOperands()}
+          <button onClick={deleteLast}>Del</button>
+        </div>
+        <div className="digits">
+          {createDigits()}
+          {displayOtherOperand()}
+          <button onClick={calculate}>=</button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
